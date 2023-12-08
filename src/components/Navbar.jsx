d4c4ed66d9c8fd265ed75a826bd74b584ea73a02
Darker689 <muhammadjon13689@gmail.com>
2023-12-08 15:15:50 +0500

    Succes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,134 @@
-import React from 'react'
+import React, { useState } from 'react'
 // React Router Dom
 import { Link } from 'react-router-dom'
+// Import icons
+import { IoMdSearch, IoMdCart, IoMdHeart, IoIosArrowDown, IoMdClose } from 'react-icons/io'
+
 
 const Navbar = () => {
+    const [search, setSearch] = useState(false);
+    const handleSearch = () => setSearch(!search);
+
+    const [isOpenPay, setIsOpenPay] = useState(false);
+    const handlePay = () => {
+        setIsOpenPay(!isOpenPay);
+    };
+
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleOptionClick = (option) => {
+        setSelectedOption(option);
+        onSelect(option.value);
+        setIsOpenPay(false);
+    };
+
+    const options = [
+        { value: '1', label: 'USD' },
+        { value: '2', label: 'RUB' },
+        { value: '3', label: 'SUM' },
+    ];
+
+    const [isOpenLang, setIsOpenLang] = useState(false);
+    const handleLang = () => {
+        setIsOpenLang(!isOpenLang);
+    };
+
+    const [selectedLang, setSelectedLang] = useState(null);
+    const handleLangClick = (lang) => {
+        setSelectedLang(lang);
+        onSelect(lang.value);
+        setIsOpenLang(false);
+    };
+
+    const languages = [
+        { value: '1', label: 'English' },
+        { value: '2', label: 'Русский' },
+        { value: '3', label: 'Uzbekcha' },
+    ];
+
     return (
         <nav>
             <div className="container">
+                <div className={!search ? 'search' : 'search_none'}>
+                    <div className="none" onClick={handleSearch}>
+                    </div>
+                    <div className="search_flex container">
+                        <Link to={'/'} className='nav_logo'>MARKT</Link>
+                        <div className="search_input">
+                            <i className='nav_i'>
+                                <IoMdSearch />
+                            </i>
+                            <input type="text" placeholder='Search' />
+                        </div>
+                        <div className="serach_close" onClick={handleSearch}>
+                            <i>
+                                <IoMdClose />
+                            </i>
+                        </div>
+                    </div>
+                </div>
                 <div className="nav_box">
                     <Link to={'/'} className='nav_logo'>MARKT</Link>
                     <div className="nav_links">
-                        <Link to={'/'}>Laptops</Link>
-                        <Link to={'/'}>Phones</Link>
-                        <Link to={'/'}>Accessories</Link>
-                        <Link to={'/'}>Acoustic</Link>
-                        <Link to={'/'}>Others</Link>
-                        <Link to={'/'}>Sale*</Link>
+                        <Link to={'/'} className='nav_link'>Laptops</Link>
+                        <Link to={'/'} className='nav_link'>Phones</Link>
+                        <Link to={'/'} className='nav_link'>Accessories</Link>
+                        <Link to={'/'} className='nav_link'>Acoustic</Link>
+                        <Link to={'/'} className='nav_link'>Others</Link>
+                        <Link to={'/'} className='nav_link nav_link_gold'>Sale*</Link>
                     </div>
                     <div className="nav_end">
-                        <div className="nav_search_btn">
+                        <div className="nav_search_btn" onClick={handleSearch}>
                             <i className='nav_i'>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                </svg>
+                                <IoMdSearch />
                             </i>
-                            search
+                            <p className='nav_search'>search</p>
                         </div>
-                        <div className="nav_icon">
+                        <Link to={'/'} className="nav_icon">
                             <i className='nav_i'>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                                </svg>
+                                <IoMdCart />
                             </i>
                             <b>0</b>
-                        </div>
-                        <div className="nav_icon">
+                        </Link>
+                        <Link to={'/'} className="nav_icon">
                             <i className='nav_i'>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                                </svg>
+                                <IoMdHeart />
                             </i>
                             <b>0</b>
-                        </div>
+                        </Link>
                         <div className="nav_usd_box">
-                            <div className="nav_usd_box_text">USD</div>
+                            <div className="nav_usd_box_text" onClick={handlePay}>{selectedOption ? selectedOption.label : 'USD'}</div>
+                            <div className={!isOpenPay ? 'nav_little_box' : 'nav_little_box_open'}>
+                                {
+                                    options.map((option) => (
+                                        <div
+                                            className={`nav_usd_box_text ${selectedOption === option ? 'selected' : ''}`}
+                                            key={option.value}
+                                            onClick={() => handleOptionClick(option)}>
+                                            {option.label}
+                                        </div>
+                                    ))
+                                }
+                            </div>
                         </div>
-                        <div className="nav_usd_box">
-                            <div className="nav_usd_box_text">
-                                EN
+                        <div className="nav_usd_box nav_lang_box">
+                            <div className="nav_usd_box_text" onClick={handleLang}>
+                                {selectedLang ? selectedLang.label : 'English'}
                                 <i className='nav_i'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                    </svg>
+                                    <IoIosArrowDown />
                                 </i>
+                            </div>
+                            <div className={!isOpenLang ? 'nav_little_box' : 'nav_little_box_open'}>
+                                {
+                                    languages.map((lang) => (
+                                        <div
+                                            className={`nav_usd_box_text ${selectedLang === lang ? 'selected' : ''}`}
+                                            key={lang.value}
+                                            onClick={() => handleLangClick(lang)}>
+                                            {lang.label}
+                                        </div>
+                                    ))
+                                }
                             </div>
                         </div>
                         <div className="nav_sing_in">
@@ -64,4 +141,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar;
