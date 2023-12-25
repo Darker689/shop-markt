3b8be1835c1384b051767793ee275a67ed42b404
Darker689 <muhammadjon13689@gmail.com>
2023-12-25 15:33:08 +0500

    KreditCart has been successfully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import MainCarusel from '../components/MainCarusel'
 import Smartphones from '../components/Smartphones'
 import Categories from '../components/Categories'
 import Laptop from '../components/Laptop'
+import KreditCart from '../components/KreditCart'
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Smartphones />
       <Categories />
       <Laptop />
+      <KreditCart />
     </>
   )
 }
